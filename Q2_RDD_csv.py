e1483d33b36209d38abb6b0500b9b1613fef3918
Giannis Vogkas <56538610+ivogkas@users.noreply.github.com>
2024-05-21 16:05:07 +0300

    Add files via upload

--- Q2_RDD_csv.py
+++ Q2_RDD_csv.py
@@ -15,39 +15,41 @@ def custom_csv_split(line):
     return next(reader)
 
 
-crimes1 = sc.textFile("Crime_Data_from_2010_to_2019.csv") \
+crimes1 = sc.textFile("Crime_Data_from_2010.csv") \
             .map(custom_csv_split)
 
-crimes2 = sc.textFile("Crime_Data_from_2020_to_Present.csv") \
+crimes2 = sc.textFile("Crime_Data_from_2020.csv") \
             .map(custom_csv_split)
 
 
-#header = crimes2.first()
-#crimes2 = crimes2.filter(lambda row: row != header) # filter out the header
+# Extract and remove header in one step for crimes1
+#header1 = crimes1.first()
 
 crimes_rdd = crimes1.union(crimes2)
 
-morning_5_to_12 = crimes_rdd.map(lambda x: x if (x[15] == "STREET" and 500 <= int(x[3]) < 1200) else None) \
-       .filter(lambda x: x != None) \
 
-afternoon_12_to_17 = crimes_rdd.map(lambda x: x if (x[15] == "STREET" and 1200 <= int(x[3]) < 1700) else None) \
-        .filter(lambda x: x != None) \
+def classify_timeslot(x):
+    if 500 <= int(x[3]) < 1200:
+        return 'Πρωί: 5.00πμ – 11.59πμ'
+    elif 1200 <= int(x[3]) < 1700:
+        return 'Απόγευμα: 12.00μμ – 4.59μμ'
+    elif 1700 <= int(x[3]) < 2100:
+        return 'Βράδυ: 5.00μμ – 8.59μμ'
+    elif int(x[3]) >= 2100 or int(x[3]) < 500:
+        return 'Νύχτα: 9.00μμ – 4.59πμ'
 
-evening_17_to_21 = crimes_rdd.map(lambda x: x if (x[15] == "STREET" and 1700 <= int(x[3]) < 2100) else None) \
-       .filter(lambda x: x != None) \
-
-night_21_to_5 = crimes_rdd.map(lambda x: x if (x[15] == "STREET" and (int(x[3]) >= 2100 or int(x[3]) < 500)) else None) \
-       .filter(lambda x: x != None) \
 
 
-dict_total_crime = {"Πρωί: 5.00πμ – 11.59πμ": morning_5_to_12.count(),
-                    "Απόγευμα: 12.00μμ – 4.59μμ": afternoon_12_to_17.count(),
-                    "Βράδυ: 5.00μμ – 8.59μμ": evening_17_to_21.count(),
-                    "Νύχτα: 9.00μμ – 4.59πμ":  night_21_to_5.count()}
+rdd = crimes_rdd.filter(lambda x : x[15] == "STREET") \
+    .map(classify_timeslot) \
+    .map(lambda timeslot: (timeslot, 1)) \
+    .reduceByKey(lambda a, b: a + b) \
+    .sortBy(lambda x: x[1], ascending=False)
 
-sorted_data = sorted(dict_total_crime.items(), key=lambda x: x[1], reverse=True)
 
-print(sorted_data)
+print(rdd.take(4))
+
+sc.stop()
 
 
 
