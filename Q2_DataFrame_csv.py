c507eb7ff473ef9600e0e73d6da1e7a5a02fc9cf
Giannis Vogkas <56538610+ivogkas@users.noreply.github.com>
2024-05-23 22:31:28 +0300

    Add files via upload

--- Q2_DataFrame_csv.py
+++ Q2_DataFrame_csv.py
@@ -52,13 +52,14 @@ crimes_df2 = spark.read.format('csv') \
 
 df = crimes_df1.union(crimes_df2)
 
-df = df.withColumn('hour', substring(df['TIME OCC'], 1, 2))
-df = df.withColumn('hour', df['hour'].cast('int'))
+#df = df.withColumn('hour', substring(df['TIME OCC'], 1, 2))
+#df = df.withColumn('hour', df['hour'].cast('int'))
 
-df = df.withColumn('timeslots',
-                   when((col("hour") >= 5) & (col("hour") < 12), 'Πρωί: 5.00πμ – 11.59πμ')
-                   .when((col("hour") >= 12) & (col("hour") < 17), 'Απόγευμα: 12.00μμ – 4.59μμ')
-                   .when((col("hour") >= 17) & (col("hour") < 21), 'Βράδυ: 5.00μμ – 8.59μμ')
+df = df.withColumn("TIME OCC", col("TIME OCC").cast("int")) \
+                .withColumn('timeslots',
+                    when((col("TIME OCC") >= 500) & (col("TIME OCC") < 1200), 'Πρωί: 5.00πμ – 11.59πμ')
+                   .when((col("TIME OCC") >= 1200) & (col("TIME OCC") < 1700), 'Απόγευμα: 12.00μμ – 4.59μμ')
+                   .when((col("TIME OCC") >= 1700) & (col("TIME OCC") < 2100), 'Βράδυ: 5.00μμ – 8.59μμ')
                    .otherwise('Νύχτα: 9.00μμ – 4.59πμ'))
 
 crime_timeslots = df.filter(df['Premis Desc'] == 'STREET')\
