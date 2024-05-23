47c558cb710959e546dbd3dc55b407b730c2b2f1
Giannis Vogkas <56538610+ivogkas@users.noreply.github.com>
2024-05-23 23:43:08 +0300

    Add files via upload

--- Q4_RDD.py
+++ Q4_RDD.py
@@ -9,6 +9,7 @@ spark = SparkSession \
     .getOrCreate() \
     .sparkContext
 
+
 def custom_csv_split(line):
     # Create a CSV reader with custom settings
     reader = csv.reader(io.StringIO(line), delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
@@ -17,45 +18,40 @@ def custom_csv_split(line):
 
 
 crimes1 = spark.textFile("Crime_Data_from_2010.csv") \
-            .map(custom_csv_split) \
-            .filter(lambda x: len(x[3]) < 4)
+    .map(custom_csv_split) \
+    .filter(lambda x: len(x[3]) < 4)
 
 crimes2 = spark.textFile("Crime_Data_from_2020.csv") \
-            .map(custom_csv_split) \
-
+    .map(custom_csv_split) \
+ \
 LAPD_rdd = spark.textFile("LAPD_Police_Stations_new.csv") \
-            .map(lambda x: (x.split(",")))
+    .map(lambda x: (x.split(",")))
 
-header_LAPD= LAPD_rdd.first()
+header_LAPD = LAPD_rdd.first()
 LAPD_rdd = LAPD_rdd.filter(lambda line: line != header_LAPD)
 
 crimes_rdd = crimes1.union(crimes2)
 
+crimes_rdd_formatted = crimes_rdd.filter(lambda x: x[26] != "0" and x[27] != "0" and len(x[16]) > 0 and x[16][0] == "1") \
+                                 .map(lambda x: [int(x[4]), [x[16], x[26], x[27]]])
 
-crimes_rdd_formatted = crimes_rdd.map(lambda x: x if (x[26] != "0" and x[27] != "0") else None) \
-                        .filter(lambda x: x != None) \
-                        .map(lambda x: x if (len(x[16]) > 0 and x[16][0] == "1") else None) \
-                        .filter(lambda x: x != None) \
-                        .map(lambda x: [int(x[4]), [x[16],x[26],x[27]]])
-
-LAPD_rdd_formatted = LAPD_rdd.map(lambda x: [int(x[3]), [x[1],x[4],x[5]]])
+LAPD_rdd_formatted = LAPD_rdd.map(lambda x: [int(x[3]), [x[1], x[4], x[5]]])
 
 joined_rdd = crimes_rdd_formatted.join(LAPD_rdd_formatted)
 
 
-def get_distance (lat1, long1, lat2, long2):
+def get_distance(lat1, long1, lat2, long2):
     return geopy.distance.geodesic((lat1, long1), (lat2, long2)).km
 
 
 joined_rdd = joined_rdd.map(lambda x: (
-     x[0],
-     x[1][0],
-     x[1][1],
-     get_distance(x[1][0][1], x[1][0][2], x[1][1][2], x[1][1][1]))) \
-     .map(lambda x: ([x[2][0], (x[3],1)])) \
-     .reduceByKey(lambda a, b: (a[0] + b[0], a[1] + b[1])) \
-     .mapValues(lambda x: (round(x[0] / x[1], 3), x[1])) \
-     .sortBy(lambda x: x[1][1], ascending=False)
-
+    x[0],
+    x[1][0],
+    x[1][1],
+    get_distance(x[1][0][1], x[1][0][2], x[1][1][2], x[1][1][1]))) \
+    .map(lambda x: ([x[2][0], (x[3], 1)])) \
+    .reduceByKey(lambda a, b: (a[0] + b[0], a[1] + b[1])) \
+    .mapValues(lambda x: (round(x[0] / x[1], 3), x[1])) \
+    .sortBy(lambda x: x[1][1], ascending=False)
 
 print(joined_rdd.take(21))
