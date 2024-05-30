a27cabe344e6ea679d33bab020c237eab44302ba
Giannis Vogkas <56538610+ivogkas@users.noreply.github.com>
2024-05-30 11:57:45 +0300

    Add files via upload

--- Q1_SQL_APIs_csv.py
+++ Q1_SQL_APIs_csv.py
@@ -1,10 +1,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructField, StructType, IntegerType, FloatType, StringType
-from pyspark.sql.functions import to_date, year, month
-from pyspark.sql.functions import col
+from pyspark.sql.functions import to_date
 from pyspark.sql.functions import col, count, month, year, rank, substring
-from pyspark.sql.window import Window
-
 
 spark = SparkSession \
     .builder \
@@ -62,7 +59,6 @@ df = df.withColumn("DATE OCC", to_date("DATE OCC", "MM/dd/yyyy hh:mm:ss a")) \
 
 # SQL view
 df.createOrReplaceTempView("crimes")
-#df.registerTempTable("crimes")
 
 query = "WITH ranked_months AS ( \
         SELECT year, month, COUNT(*) AS crime_total, ROW_NUMBER() OVER (PARTITION BY year ORDER BY COUNT(*) DESC) AS ranking \
@@ -73,7 +69,6 @@ query = "WITH ranked_months AS ( \
         FROM ranked_months \
         WHERE ranking <= 3 \
         ORDER BY year ASC, crime_total DESC;"
-
 
 result = spark.sql(query)
 
